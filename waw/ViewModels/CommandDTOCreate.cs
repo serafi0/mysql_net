3323201eeade194af18681c7daa009c472f0c632
mohamedelserafy <elserafy02@gmail.com>
2021-02-08 03:14:06 +0200

    small edit

--- waw/ViewModels/CommandDTOCreate.cs
+++ waw/ViewModels/CommandDTOCreate.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace waw.ViewModels
 {
     public class CommandDTOCreate
     {
         //public int Id { get; set; } we don't need an id in the  Create model View
+        [Required]
         public string Howto { get; set; }
+        [Required]
         public string Commandline { get; set; }
+        [Required]
         public string Platform { get; set; }
 
 
